1495555984094ce038f19378b074daf9d3210ed3
ayush tiwari <76848944+ayushtiwari88@users.noreply.github.com>
2021-08-10 18:00:22 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-print("what is your age ?")
-age= int(input())
-if 0<age<18:
-    print("you can not drive")
-elif age== 18:
-    print("we will think about it")
-else:
-    print("you can drive")        
